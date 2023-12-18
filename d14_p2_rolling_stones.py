ce382102130ed99ac3d9dc5ddede17a6c5307251
JakubFlash <j-k@op.pl>
2023-12-18 15:20:18 +0100

    part 2 of day 14 cleaned up

--- d14_p2_rolling_stones.py
+++ d14_p2_rolling_stones.py
@@ -12,28 +12,20 @@ def summarize_load(layout : list[str]) -> int:
     return total_load
         
 def tilt(layout : list[str], direction : int) -> list[str]:
-    clockwise_rotations = {
-        "N" : 0,
-        "W" : 1,
-        "S" : 2,
-        "E" : 3
-    }
+    """
+        directions: N=0, W=1, S=2, E=3
 
-    return_rotations = {
-        "N" : 0,
-        "W" : 3,
-        "S" : 2,
-        "E" : 1
-    }
-    # directions: N=0, W=1, S=2, E=3
-    resulting_layout = []
-    # rotate for direction to face upwards
-    # transpose
-    # per each line:
-    # '#' as substring divisor
-    # substrings sorted reverse alphabetically
-    # un-transpose
-    # un-rotate
+        flow:
+        rotate for (direction) to face upwards
+            transpose
+            per each line:
+                split into substring using '#' as separator
+                    substrings sorted reverse alphabetically (OOO....)
+                substrings connected with # as separator
+            un-transpose
+        un-rotate
+    """
+
     for i in range(direction):
         rotated = []
         rotated = [''.join(line) for line in zip(*reversed(layout))]
@@ -62,45 +54,57 @@ with open("input.txt") as input_file:
     input_ = input_file.read() 
 
 input_lines = input_.split('\n')
-input_lines_form_zero = input_lines
-
-permutations_encountered = set()
 
 total_cycles = 1000000000
+permutations_encountered = set()
 
-for cycle in range(total_cycles):
+for start_to_form_A in range(total_cycles):
     
     for i in range(4):
         input_lines = tilt(input_lines, i)
     
-
     if tuple(input_lines) in permutations_encountered:
-        print(f"repetition after cycle no. {cycle}")
+        print(f"repetition after cycle no. {start_to_form_A}")
         A_form = input_lines
         break
     permutations_encountered.add(tuple(input_lines))
 
-# locate offset
-#  zero>| offset | A_form | (cycle - offset = repetitions) | A_form
-input_lines = input_lines_form_zero
-for c in range(cycle+1):
+"""
+A_form = form of layout found to repeat, i.e. one which starts an infinite loop
+zero = starting form of layout
+|n| = n cycles
+
+form: zero
+    | start_to_form_A |
+A_form 
+    | repetition |   ^
+A_form               |
+    | repetition |   |
+        | ... |      | X
+        | ... |      |
+        | ... |      |
+    | repetition |   V
+A_form 
+    | final_cycles |
+final_form
+
+total_cycles = start_to_form_A + X * repetitions + final_cycles
+
+final_cycles = (total_cycles - start_to_form_A) % repetitions
+
+"""
+for repetition in range(start_to_form_A):
     for i in range(4):
         input_lines = tilt(input_lines, i)
     if tuple(input_lines) == tuple(A_form):
-        print(f"OK, offset = {c}")
         break
-print("is it ok?")
-# (długość cyklu to tyle ile od start form do A form)
-offset = c
-repetitions = cycle - offset
-# tysiąc minus ofset modulo długość cyklu
-final_cycles = (total_cycles - offset) % repetitions
+start_to_form_A += 1 # adjust for cycle count starting with 0
+final_cycles = (total_cycles - start_to_form_A) % repetition
 
-for cycle in range(final_cycles - 1):
+for cycle in range(final_cycles):
     for i in range(4):
         input_lines = tilt(input_lines, i)
 
 print(*input_lines, sep='\n')
 print(summarize_load(input_lines))
 print(f"total time: {time.time() - start_time}")
-
